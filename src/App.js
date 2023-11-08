20a5cf00cfcd9d9330fd4d9720f13d4a94c9a55b
Cherono-mercy <cheronom142@gmail.com>
2023-11-08 20:43:54 +0800

    Setting up project environment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css";
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className='App'>
+     
+    </div>
+  )
+}
+
+export default App
